27389c8d8116f4f8754d9678f813433cbfe163c6
sebastian <sebastian@sommerfeld.io>
2022-11-21 23:11:35 +0100

    feat: added installation script

---
+++ src/main/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# @file install.sh
+# @brief Install jarvis to ``/opt/jarvis`` and make executable via ``/usr/bin/jarvis``.
+#
+# @description The script installs jarvis to ``/opt/jarvis`` and makes jarvis executable via ``/usr/bin/jarvis``.
+# All jarvis assets are cloned from Github during the installation.
+#
+# === Script Arguments
+#
+# The script does not accept any parameters.
+#
+# === Script Example
+#
+# [source, bash]
+# ```
+# ./install.sh
+# # or
+# curl https://raw.githubusercontent.com/sebastian-sommerfeld-io/jarvis/main/src/main/install.sh | bash -
+# ```
+
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+
+TEMP_PATH="/tmp/jarvis"
+REPO_PATH="/opt/jarvis"
+BIN="/usr/bin/jarvis"
+
+
+echo -e "$LOG_INFO Clone Jarvis Repository"
+git clone git@github.com:sebastian-sommerfeld-io/jarvis.git "$TEMP_PATH"
+sudo mv "$TEMP_PATH" "$REPO_PATH"
+
+echo -e "$LOG_INFO Clone Jarvis Repository"
+sudo ln -s "$REPO_PATH/src/main/jarvis.sh" "$BIN"
+chmod +x "$BIN"
+
+echo -e "$LOG_INFO Jarvis setup complete"
--- src/main/jarvis.sh
+++ src/main/jarvis.sh
@@ -25,11 +25,19 @@ set -o nounset
 # set -o xtrace
 
 
+MODULES_PATH=""
+if [ "$0" = "/usr/bin/jarvis" ]; then
+  MODULES_PATH="/opt/jarvis/src/main/"
+fi
+
+
 docker run --rm mwendler/figlet:latest 'Jarvis CLI'
 echo -e "$LOG_INFO Current workdir = $(pwd)"
 echo -e "$LOG_INFO What do you want me to do?"
-select module in modules/*; do
+select module in "$MODULES_PATH"modules/*; do
   echo -e "$LOG_INFO Run $P$module$D"
+
   bash "$module/module.sh" "$module"
+  
   break
 done
