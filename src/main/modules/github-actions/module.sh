840ef813a4f08c21afdbb180b058e6ba4cd288c5
sebastian <sebastian@sommerfeld.io>
2022-12-06 21:22:38 +0100

    feat: Auto link all issues without a project - #7

--- src/main/modules/github-actions/module.sh
+++ src/main/modules/github-actions/module.sh
@@ -54,4 +54,5 @@ cp "$1/assets/PULL_REQUEST_TEMPLATE.md" "PULL_REQUEST_TEMPLATE.md"
   cp -a "$1/assets/workflows/assets" "assets"
   cp "$1/assets/workflows/organize-auto-close-issues.yml" "organize-auto-close-issues.yml"
   cp "$1/assets/workflows/organize-labels.yml" "organize-labels.yml"
+  cp "$1/assets/workflows/organize-assign-issues.yml" "organize-assign-issues.yml"
 )
