aad10eada559390e225a5ff81c932b4d37c3f292
sebastian <sebastian@sommerfeld.io>
2023-04-04 14:20:19 +0200

    no more separate module file anymore

--- src/main/modules/bash-new-script/module.sh
+++ src/main/modules/bash-new-script/module.sh
@@ -25,7 +25,53 @@ set -o nounset
 # set -o xtrace
 
 
-source lib/log.sh
+#source lib/log.sh
+
+
+# @description Log message with log level = DONE.
+#
+# @arg $@ String The line to print.
+function LOG_DONE() {
+  local LOG_DONE="[\e[32mDONE\e[0m]"
+  echo -e "$LOG_DONE $1"
+}
+
+
+# @description Log message with log level = ERROR.
+#
+# @arg $@ String The line to print.
+function LOG_ERROR() {
+  local LOG_ERROR="[\e[1;31mERROR\e[0m]" 
+  echo -e "$LOG_ERROR $1"
+}
+
+
+# @description Log message with log level = INFO.
+#
+# @arg $@ String The line to print.
+function LOG_INFO() {
+  local LOG_INFO="[\e[34mINFO\e[0m]"
+  echo -e "$LOG_INFO $1"
+}
+
+
+# @description Log message with log level = WARN.
+#
+# @arg $@ String The line to print.
+function LOG_WARN() {
+  local LOG_WARN="[\e[93mWARN\e[0m]"
+  echo -e "$LOG_WARN $1"
+}
+
+
+# @description Print log output in a header-style.
+#
+# @arg $@ String The line to print.
+function LOG_HEADER() {
+  LOG_INFO "------------------------------------------------------------------------------------"
+  LOG_INFO "$1"
+  LOG_INFO "------------------------------------------------------------------------------------"
+}
 
 
 if [ -z "$1" ]; then
