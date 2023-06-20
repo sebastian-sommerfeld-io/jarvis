3adca22179d440f88fb1a79dc869bee01b0dce26
sebastian <sebastian@sommerfeld.io>
2023-06-20 22:43:39 +0200

    added logging snippet to bash template

--- src/main/modules/bash-script/assets/template.sh
+++ src/main/modules/bash-script/assets/template.sh
@@ -23,4 +23,11 @@ set -o nounset
 # set -o xtrace
 
 
+# Download and include logging library
+rm -rf /tmp/bash-lib
+mkdir -p /tmp/bash-lib
+curl -sL https://raw.githubusercontent.com/sebastian-sommerfeld-io/jarvis/main/src/main/modules/bash-script/assets/lib/log.sh --output /tmp/bash-lib/log.sh
+source /tmp/bash-lib/log.sh
+
+
 echo -e "$LOG_INFO Run $0"
